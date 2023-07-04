52da7cc58463db19ebca1aab49b1c8211d22cae5
Arun <arun.balasubramanian@freshworks.com>
2023-07-04 08:39:29 +0530

    remove unwanted files

---
+++ src/test/scala/videogamedb/MyFirstTest.scala
@@ -0,0 +1,12 @@
+package videogamedb
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+class MyFirstTest extends Simulation {
+
+  // 1. HTTP CONFIGURATION
+  val httpProtocol = new httpProtocol()
+
+  // 2. SCENARIO
+
+}
