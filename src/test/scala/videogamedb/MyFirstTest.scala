45e899685d31b98be67b66de18a5dbec3710978a
Arun <arun.balasubramanian@freshworks.com>
2023-07-04 08:55:31 +0530

    first test with gatling

--- src/test/scala/videogamedb/MyFirstTest.scala
+++ src/test/scala/videogamedb/MyFirstTest.scala
@@ -2,11 +2,25 @@ package videogamedb
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
 class MyFirstTest extends Simulation {
 
   // 1. HTTP CONFIGURATION
-  val httpProtocol = new httpProtocol()
+  val httpProtocol = http.baseUrl("https://catfact.ninja")
+    .acceptHeader("application/json")
 
-  // 2. SCENARIO
+
+  // 2. SCENARIO CONFIGURATION
+  val scn = scenario("Get list of facts and a fact")
+    .exec(http("GET ALL FACTS")
+    .get("/facts"))
+    .exec(http("GET one FACT")
+    .get("/fact"))
+
+  // 3. LOAD SCENARIO
+  setUp(
+    scn.inject(atOnceUsers(1))
+      .protocols(httpProtocol)
+  )
 
 }
