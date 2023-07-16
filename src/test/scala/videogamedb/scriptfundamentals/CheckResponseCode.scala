3117273a76c3895ba4cf25165d518bafe5cce7b1
Arun <arun.balasubramanian@freshworks.com>
2023-07-16 12:05:55 +0530

    Script Fundamentals - CheckResponseCode

--- src/test/scala/videogamedb/scriptfundamentals/CheckResponseCode.scala
+++ src/test/scala/videogamedb/scriptfundamentals/CheckResponseCode.scala
@@ -4,23 +4,25 @@ import io.gatling.http.Predef._
 import io.gatling.core.Predef._
 
 import scala.concurrent.duration.DurationInt
-
-class AddPauseTime extends Simulation {
+class CheckResponseCode extends Simulation {
 
   val httpProtocol = http.baseUrl("https://catfact.ninja")
     .acceptHeader("application/json")
 
-  val scn = scenario("Get Facts")
+  val scn = scenario("Check response code")
     .exec(http("Get all facts - 1")
-      .get("/facts"))
+      .get("/facts")
+      .check(status.is(200)))
     .pause(2)
 
     .exec(http("Get one fact - 1")
-      .get("/fact"))
+      .get("/fact")
+      .check(status.in(200, 210)))
     .pause(3, 10)
 
     .exec(http("Get all facts - 2")
-      .get("/facts"))
+      .get("/facts")
+      .check(status.not(500), status.not(400)))
     .pause(2000.milliseconds)
 
     .exec(http("Get one fact - 2")
@@ -28,6 +30,6 @@ class AddPauseTime extends Simulation {
 
   setUp(
     scn.inject(atOnceUsers(1))
-    .protocols(httpProtocol)
+      .protocols(httpProtocol)
   )
 }
